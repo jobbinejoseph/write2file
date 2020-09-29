7cb7fa403a03512a994b60f84e3c61583141c55c
jobbinejoseph <54904120+jobbinejoseph@users.noreply.github.com>
2020-09-29 15:23:57 -0400

    Update ut.py

--- ut.py
+++ ut.py
@@ -2,10 +2,13 @@ import unittest
 from load import load
 
 class AddTest(unittest.TestCase):
-    def test_addition(self):
+    def setUp(self):
         module = load('writenew')
+    def test_addition(self):
         self.assertEqual(module.main_fun(1,"job.txt"), 2)
 
+    def test_addition(self):
+        self.assertEqual(module.main_fun(2,"job.txt"), 3)
 
 if __name__ == "__main__":
     unittest.main()
